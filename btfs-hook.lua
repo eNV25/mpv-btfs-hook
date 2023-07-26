76061c405f5a383f9034fd0a05f2b484a58078ee
eNV25 <env252525@gmail.com>
2023-07-26 12:33:04 +0200

    My formatter told me

--- btfs-hook.lua
+++ btfs-hook.lua
@@ -24,15 +24,15 @@ local btfs_args = {
 
 	-- these are in kB/s
 	--'--max-download-rate=4900',
-	'--max-upload-rate=500',
+	"--max-upload-rate=500",
 }
 
-local mountdir = '/tmp/mpvbtfs'
+local mountdir = "/tmp/mpvbtfs"
 
 -- list files from the mountpoint that should added to the playlist
-local list_files = function (mountpoint)
+local list_files = function(mountpoint)
 	local p = assert(io.popen([[
-	mountpoint=]]..shellquote(mountpoint)..'\n'..[[
+	mountpoint=]] .. shellquote(mountpoint) .. "\n" .. [[
 	# -V = version sort. should sort anime episodes correctly
 	find "$mountpoint" -type f | sort -V
 	]]))
@@ -43,8 +43,10 @@ local list_files = function (mountpoint)
 	end
 	p:close()
 	-- put files before directories but keep the sort order
-	local count_slashes = function (path) return #path-#path:gsub('/', '') end
-	table.sort(files, function (p1, p2)
+	local count_slashes = function(path)
+		return #path - #path:gsub("/", "")
+	end
+	table.sort(files, function(p1, p2)
 		local c1 = count_slashes(p1)
 		local c2 = count_slashes(p2)
 		if c1 ~= c2 then
@@ -58,13 +60,13 @@ end
 
 --------------------------------------------------------------------------------
 
-shellquote = function (s)
-	return '\'' .. s:gsub('\'', '\'\\\'\'') .. '\''
+shellquote = function(s)
+	return "'" .. s:gsub("'", "'\\''") .. "'"
 end
 
 local exec_ok = os.execute
-if _VERSION == 'Lua 5.1' then
-	exec_ok = function (...)
+if _VERSION == "Lua 5.1" then
+	exec_ok = function(...)
 		return 0 == os.execute(...)
 	end
 end
@@ -74,32 +76,33 @@ end
 -- mountpoints mounted by us (will be unmounted on shutdown)
 local mounted_points = {}
 
-local do_unmount = function (mountpoint)
+local do_unmount = function(mountpoint)
 	os.execute([[
-	mountpoint=]]..shellquote(mountpoint)..'\n'..[[
+	mountpoint=]] .. shellquote(mountpoint) .. "\n" .. [[
 	fusermount -u "$mountpoint"
 	rmdir "$mountpoint"
 	]])
 	mounted_points[mountpoint] = nil
 end
 
-local do_mount = function (url, mountpoint)
-	if type(btfs_args) == 'table' then
+local do_mount = function(url, mountpoint)
+	if type(btfs_args) == "table" then
 		for i = 1, #btfs_args do
 			btfs_args[i] = shellquote(btfs_args[i])
 		end
-		btfs_args = table.concat(btfs_args, ' ')
+		btfs_args = table.concat(btfs_args, " ")
 	end
-	local title = ('btfs - '..mountpoint:match('[^/]+$'))
-	if not exec_ok([[
-	mountpoint=]]..shellquote(mountpoint)..'\n'..[[
-	url=]]..shellquote(url)..'\n'..[[
+	local title = ("btfs - " .. mountpoint:match("[^/]+$"))
+	if
+			not exec_ok([[
+	mountpoint=]] .. shellquote(mountpoint) .. "\n" .. [[
+	url=]] .. shellquote(url) .. "\n" .. [[
 	mkdir -p "$mountpoint" || exit 1
 	{
 	# if command -v xterm >/dev/null; then
-	# 	exec xterm -title ]]..shellquote(title)..[[ -e btfs -f ]]..btfs_args..[[ "$url" "$mountpoint"
+	# 	exec xterm -title ]] .. shellquote(title) .. [[ -e btfs -f ]] .. btfs_args .. [[ "$url" "$mountpoint"
 	# else
-		exec btfs -f ]]..btfs_args..[[ "$url" "$mountpoint" >/dev/null 2>&1
+		exec btfs -f ]] .. btfs_args .. [[ "$url" "$mountpoint" >/dev/null 2>&1
 	#fi
 	} &
 	pid=$!
@@ -115,27 +118,28 @@ local do_mount = function (url, mountpoint)
 		fi
 		command sleep 0.25 || exit 1
 	done
-	]]) then
+	]])
+	then
 		return false
 	end
 	mounted_points[mountpoint] = true
 	return true
 end
 
-local is_mounted = function (mountpoint)
-	return exec_ok('mountpoint -q '..shellquote(mountpoint))
+local is_mounted = function(mountpoint)
+	return exec_ok("mountpoint -q " .. shellquote(mountpoint))
 end
 
 --------------------------------------------------------------------------------
 
 -- gets the info hash or torrent filename for use as the mount directory name
-local parse_url = function (url)
-	return url:match('^magnet:.*[?&]xt=urn:bt[im]h:([a-zA-Z0-9]*)&?')
-	    or url:gsub('[?#].*', '', 1):match('/([^/]+%.torrent)$')
+local parse_url = function(url)
+	return url:match("^magnet:.*[?&]xt=urn:bt[im]h:([a-zA-Z0-9]*)&?")
+			or url:gsub("[?#].*", "", 1):match("/([^/]+%.torrent)$")
 end
 
-mp.add_hook('on_load', 11, function ()
-	local url = mp.get_property('stream-open-filename')
+mp.add_hook("on_load", 11, function()
+	local url = mp.get_property("stream-open-filename")
 	if not url then
 		return
 	end
@@ -145,31 +149,31 @@ mp.add_hook('on_load', 11, function ()
 		return
 	end
 
-	local mountpoint = (mountdir..'/'..dirname)
+	local mountpoint = (mountdir .. "/" .. dirname)
 	if not is_mounted(mountpoint) then
 		if not do_mount(url, mountpoint) then
-			print('mount failed!')
+			print("mount failed!")
 			return
 		end
 	end
 
 	local files = list_files(mountpoint)
 	if #files == 0 then
-		print('nothing to play!')
+		print("nothing to play!")
 	elseif #files == 1 then
-		mp.set_property("file-local-options/force-media-title", files[1]:match('[^/]+$'))
-		mp.set_property('stream-open-filename', 'file://'..files[1])
+		mp.set_property("file-local-options/force-media-title", files[1]:match("[^/]+$"))
+		mp.set_property("stream-open-filename", "file://" .. files[1])
 	else
-		local playlist = {'#EXTM3U'}
+		local playlist = { "#EXTM3U" }
 		for _, line in ipairs(files) do
-			table.insert(playlist, '#EXTINF:0,'..line:match('[^/]+$'))
-			table.insert(playlist, 'file://'..line)
+			table.insert(playlist, "#EXTINF:0," .. line:match("[^/]+$"))
+			table.insert(playlist, "file://" .. line)
 		end
-		mp.set_property('stream-open-filename', 'memory://'..table.concat(playlist, '\n'))
+		mp.set_property("stream-open-filename", "memory://" .. table.concat(playlist, "\n"))
 	end
 end)
 
-mp.register_event('shutdown', function ()
+mp.register_event("shutdown", function()
 	for mountpoint in pairs(mounted_points) do
 		do_unmount(mountpoint)
 	end
