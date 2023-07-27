45b298f6938fc840ef617ab634faa055394ba027
eNV25 <env252525@gmail.com>
2023-07-27 22:12:09 +0200

    Rework rest of script to not use sh.

--- btfs-hook.lua
+++ btfs-hook.lua
@@ -7,7 +7,12 @@ original script: https://gist.github.com/huglovefan/4c68bc40661b6701ca5fc6ce1157
 requires:
 - linux
 - btfs
-- xterm (optional)
+- file --brief --mime-type
+- mountpoint -q
+- umount
+- rmdir
+- mkdir -p
+- sleep
 
 usage:
 - open a magnet link or torrent url using mpv and it should Just Work
@@ -33,22 +38,12 @@ local mountdir = "/tmp/mpvbtfs"
 
 local mp = require("mp")
 local utils = require("mp.utils")
-
-local shellquote = function(s)
-	return "'" .. s:gsub("'", [['\'']]) .. "'"
-end
-
-local exec_ok = os.execute
-if _VERSION == "Lua 5.1" then
-	exec_ok = function(...)
-		return 0 == os.execute(...)
-	end
-end
+local msg = require("mp.msg")
 
 local MPV_MIME_TYPES = {};
 
 (function() -- init MPV_MIME_TYPES
-	-- get XDG_DATA_DIRS and add a final semicolon to make it easier to parse
+	-- get XDG_DATA_DIRS and add a final colon to make it easier to parse
 	local XDG_DATA_DIRS = os.getenv("XDG_DATA_DIRS") or "/usr/share"
 	if not XDG_DATA_DIRS:match(":$") then
 		XDG_DATA_DIRS = XDG_DATA_DIRS .. ":"
@@ -135,66 +130,44 @@ end
 -- mountpoints mounted by us (will be unmounted on shutdown)
 local mounted_points = {}
 
-local do_unmount = function(mountpoint)
-	os.execute([[
-	mountpoint=]] .. shellquote(mountpoint) .. "\n" .. [[
-	fusermount -u "$mountpoint"
-	rmdir "$mountpoint"
-	]])
-	mounted_points[mountpoint] = nil
+local is_mounted = function(mountpoint)
+	return mp.command_native({ name = "subprocess", args = { "mountpoint", "-q", mountpoint } }).status == 0
+end
+
+local do_unmount = function(url, mountpoint)
+	mounted_points[url] = nil
+	mp.command_native({ name = "subprocess", args = { "umount", mountpoint }, playback_only = false })
+	mp.command_native({ name = "subprocess", args = { "rmdir", mountpoint }, playback_only = false })
 end
 
 local do_mount = function(url, mountpoint)
-	if type(btfs_args) == "table" then
-		for i = 1, #btfs_args do
-			btfs_args[i] = shellquote(btfs_args[i])
-		end
-		btfs_args = table.concat(btfs_args, " ")
-	end
-	local title = ("btfs - " .. mountpoint:match("[^/]+$"))
-	if
-			not exec_ok([[
-	mountpoint=]] .. shellquote(mountpoint) .. "\n" .. [[
-	url=]] .. shellquote(url) .. "\n" .. [[
-	mkdir -p "$mountpoint" || exit 1
-	{
-	# if command -v xterm >/dev/null; then
-	# 	exec xterm -title ]] .. shellquote(title) .. [[ -e btfs -f ]] .. btfs_args .. [[ "$url" "$mountpoint"
-	# else
-		exec btfs -f ]] .. btfs_args .. [[ "$url" "$mountpoint" >/dev/null 2>&1
-	#fi
-	} &
-	pid=$!
-	while true; do
-		if [ ! -e /proc/$pid ]; then
-			exit 1
-		fi
-		if mountpoint -q "$mountpoint"; then
-			set -- "$mountpoint"/*
-			if [ $# -gt 1 ] || [ -e "$1" ]; then
-				exit 0
-			fi
-		fi
-		command sleep 0.25 || exit 1
-	done
-	]])
-	then
-		return false
-	end
-	mounted_points[mountpoint] = true
-	return true
-end
+	mp.command_native({ name = "subprocess", args = { "mkdir", "-p", mountpoint } })
 
-local is_mounted = function(mountpoint)
-	return exec_ok("mountpoint -q " .. shellquote(mountpoint))
+	mp.command_native({ name = "subprocess", args = { "btfs", url, mountpoint } })
+	mounted_points[url] = mountpoint
+
+	msg.verbose("waiting for files")
+
+	-- wait until btfs is finished mounting, else fail
+	while is_mounted(mountpoint) do
+		if #utils.readdir(mountpoint) > 0 then
+			msg.verbose("files found")
+			return true
+		end
+		mp.command_native({ name = "subprocess", args = { "sleep", "0.25" } })
+	end
+	return false
 end
 
 --------------------------------------------------------------------------------
 
 -- gets the info hash or torrent filename for use as the mount directory name
 local parse_url = function(url)
-	return url:match("^magnet:.*[?&]xt=urn:bt[im]h:([a-zA-Z0-9]*)&?")
-			or url:gsub("[?#].*", "", 1):match("/([^/]+%.torrent)$")
+	return url:match("^magnet:.*[?&]xt=urn:bt[im]h:(%w*)&?") or url:gsub("[?#].*", "", 1):match("/([^/]+%.torrent)$")
+end
+
+local file_url = function(file)
+	return "file://" .. file:gsub("%%", "%%25"):gsub("\r", "%%0D"):gsub("\n", "%%0A")
 end
 
 mp.add_hook("on_load", 11, function()
@@ -203,37 +176,38 @@ mp.add_hook("on_load", 11, function()
 		return
 	end
 
+	msg.verbose("using url " .. url)
+
 	local dirname = parse_url(url)
 	if not dirname then
 		return
 	end
 
 	local mountpoint = (mountdir .. "/" .. dirname)
+
+	msg.verbose("using mountpoint " .. mountpoint)
+
 	if not is_mounted(mountpoint) then
 		if not do_mount(url, mountpoint) then
-			print("mount failed!")
+			msg.error("mount failed!")
 			return
 		end
 	end
 
 	local files = list_files(mountpoint)
 	if #files == 0 then
-		print("nothing to play!")
-	elseif #files == 1 then
-		mp.set_property("file-local-options/force-media-title", files[1]:match("[^/]+$"))
-		mp.set_property("stream-open-filename", "file://" .. files[1])
+		msg.error("nothing to play!")
 	else
 		local playlist = { "#EXTM3U" }
-		for _, line in ipairs(files) do
-			table.insert(playlist, "#EXTINF:0," .. line:match("[^/]+$"))
-			table.insert(playlist, "file://" .. line)
+		for _, file in ipairs(files) do
+			table.insert(playlist, file_url(file))
 		end
 		mp.set_property("stream-open-filename", "memory://" .. table.concat(playlist, "\n"))
 	end
 end)
 
 mp.register_event("shutdown", function()
-	for mountpoint in pairs(mounted_points) do
-		do_unmount(mountpoint)
+	for url, mountpoint in pairs(mounted_points) do
+		do_unmount(url, mountpoint)
 	end
 end)
